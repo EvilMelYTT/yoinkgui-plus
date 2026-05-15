a357fbda15b7b2a886eaa717f60d0984d86cedb8
EvilMelYTT <89437272+EvilMelYTT@users.noreply.github.com>
2026-05-15 18:52:59 -0400

    Add files via upload

--- src/client/kotlin/me/thatonedevil/YoinkGUIClient.kt
+++ src/client/kotlin/me/thatonedevil/YoinkGUIClient.kt
@@ -2,18 +2,20 @@ package me.thatonedevil
 
 import me.thatonedevil.commands.YoinkGuiClientCommandRegistry
 import me.thatonedevil.config.YoinkGuiSettings
-import me.thatonedevil.handlers.SingleItemYoinkHandler
 import me.thatonedevil.handlers.ParseButtonHandler
 import me.thatonedevil.keybinds.KeybindManager
 import me.thatonedevil.utils.api.UpdateChecker
 import net.fabricmc.api.ClientModInitializer
+import net.minecraft.client.KeyMapping
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-//? if >=1.21.9 {
+//? if >=1.21.4 {
 import net.minecraft.resources.Identifier
-import net.minecraft.client.KeyMapping
-//? }
+//?} else {
+/*import net.minecraft.resources.ResourceLocation
+*/
+//?}
 
 object YoinkGUIClient : ClientModInitializer {
     val logger: Logger = LoggerFactory.getLogger(BuildConfig.MOD_ID)
@@ -21,20 +23,18 @@ object YoinkGUIClient : ClientModInitializer {
     @JvmStatic
     val yoinkGuiSettings: YoinkGuiSettings = YoinkGuiSettings
 
-    //? if >=1.21.9 {
+    //? if >=1.21.4 {
     val keybindCategory: KeyMapping.Category = KeyMapping.Category.register(Identifier.parse("keybinds"))
-    //? }
+    //?} else {
+    /*val keybindCategory: KeyMapping.Category = KeyMapping.Category.register(ResourceLocation.parse("keybinds"))
+    */
+    //?}
 
     override fun onInitializeClient() {
-
         UpdateChecker.setupJoinListener()
         YoinkGuiClientCommandRegistry.register()
         KeybindManager().register()
-
-        yoinkGuiSettings // Load settings on client init
-
-        // Register event handlers
+        yoinkGuiSettings
         ParseButtonHandler.register()
-        SingleItemYoinkHandler.register()
     }
 }
